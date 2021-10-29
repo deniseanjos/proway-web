959051db11d4b673e4f3d8db85d31b116d229f0f
Denise Anjos <deniseb.anjos@outlook.com>
2021-10-29 18:02:35 -0300

    atividade pratica 35

---
+++ html-css-js/atp35/js/index.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+    resumo();
+}
+
+function resumo(){
+    let frame = document.getElementById("frame");
+    frame.src="resumo.html";
+}
+
+function experiencias(){
+    let frame = document.getElementById("frame");
+    frame.src="experiencia.html";
+}
+
+function habilidades(){
+    let frame = document.getElementById("frame");
+    frame.src="habilidade.html";
+}
+
+function contatos(){
+    let frame = document.getElementById("frame");
+    frame.src="contato.html";
+}
